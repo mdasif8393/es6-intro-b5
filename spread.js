64cd27efc52ebd346d03d53ee740aa21c2e0f4bc
mdasif8393 <md.asif.8393@gmail.com>
2022-08-17 16:23:01 +0600

    commit 3 file

---
+++ spread.js
@@ -0,0 +1,7 @@
+const numbers = [12, 43, 34, 54, 65];
+// console.log(numbers);
+// console.log(...numbers);
+// console.log(Math.max(...numbers))
+
+const newNumbers = [555, 666, 444, ...numbers];
+console.log(newNumbers);
---
+++ template-string.js
@@ -0,0 +1,11 @@
+const a = 18;
+const routine = `Ami shokale uthi
+Bikale khaite ${a}jai
+ratte moja kori
+`
+
+const first = "Mamun";
+const second = "Hasan";
+
+const full = `${first} ${second}`
+console.log(full)
